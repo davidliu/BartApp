efe47594e9bfd9ba978d159a838bb3674bddaf70
David Liu <dliu@medium.com>
2019-09-02 20:06:59 -0700

    Main Activity cleanup

--- app/src/main/java/com/deviange/bart/activity/MainActivity.kt
+++ app/src/main/java/com/deviange/bart/activity/MainActivity.kt
@@ -2,7 +2,6 @@ package com.deviange.bart.activity
 
 import android.app.Activity
 import android.os.Bundle
-import android.util.Log
 import com.deviange.bart.R
 import com.deviange.bart.activity.dagger.BaseActivityModule
 import dagger.Binds
@@ -14,15 +13,6 @@ class MainActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        Log.e("LOL", "asdf")
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
-        super.onRestoreInstanceState(savedInstanceState)
     }
 }
 
