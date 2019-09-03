a9e64de05d2d8d1247c8340e4a4545c98ce7d079
David Liu <dliu@medium.com>
2019-09-02 20:48:23 -0700

    Show back arrow if needed

--- app/src/main/java/com/deviange/bart/activity/MainActivity.kt
+++ app/src/main/java/com/deviange/bart/activity/MainActivity.kt
@@ -2,17 +2,33 @@ package com.deviange.bart.activity
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.MenuItem
 import com.deviange.bart.R
 import com.deviange.bart.activity.dagger.BaseActivityModule
 import dagger.Binds
 import dagger.Module
 import dagger.android.support.DaggerAppCompatActivity
 
+
 class MainActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+        supportFragmentManager.addOnBackStackChangedListener {
+            supportActionBar?.setDisplayHomeAsUpEnabled(supportFragmentManager.backStackEntryCount > 0)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.getItemId()
+        if (id == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
 
