ebc83a7f375da41d5656cbaa586a02b24c955205
David Liu <dliu@medium.com>
2019-09-04 21:07:55 -0700

    Add estimated time

--- app/src/main/java/com/deviange/bart/stations/estimates/DepartureItem.kt
+++ app/src/main/java/com/deviange/bart/stations/estimates/DepartureItem.kt
@@ -3,29 +3,47 @@ package com.deviange.bart.stations.estimates
 import android.graphics.Color
 import android.view.View
 import com.deviange.bart.R
-import com.xwray.groupie.Item
-import com.xwray.groupie.ViewHolder
-import kotlinx.android.synthetic.main.departure_item.view.*
+import com.xwray.groupie.kotlinandroidextensions.Item
+import com.xwray.groupie.kotlinandroidextensions.ViewHolder
+import kotlinx.android.synthetic.main.departure_item.*
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class DepartureItem(
     private val departure: RouteDeparture,
     private val onClick: View.OnClickListener?
-) : Item<ViewHolder>() {
+) : Item() {
     override fun getLayout(): Int = R.layout.departure_item
 
     override fun bind(viewHolder: ViewHolder, position: Int) {
         val context = viewHolder.root.context
-        viewHolder.root.destination.text = departure.route.destination
-        viewHolder.root.estimate.text = when (departure.departure.minutes) {
-            "Leaving" -> context.getString(R.string.leaving)
-            else -> context.getString(R.string.departure_in_minutes, departure.departure.minutes)
+        viewHolder.destination.text = departure.route.destination
+        when (departure.departure.minutes) {
+            "Leaving" -> {
+                viewHolder.estimate.text = context.getString(R.string.leaving)
+                viewHolder.time.visibility = View.GONE
+            }
+            else -> {
+                viewHolder.estimate.text = context.getString(R.string.departure_in_minutes, departure.departure.minutes)
+
+                val minutes = departure.departure.minutes.toLong()
+                val departureTime = Date(Date().time + TimeUnit.MILLISECONDS.convert(minutes, TimeUnit.MINUTES))
+                viewHolder.time.visibility = View.VISIBLE
+                viewHolder.time.text = dateFormatter.format(departureTime)
+            }
         }
-        viewHolder.root.color_indicator.setBackgroundColor(Color.parseColor(departure.departure.hexcolor))
+        viewHolder.color_indicator.setBackgroundColor(Color.parseColor(departure.departure.hexcolor))
         viewHolder.root.setOnClickListener(onClick)
     }
 
-    override fun isSameAs(other: Item<*>?): Boolean {
+
+    override fun isSameAs(other: com.xwray.groupie.Item<*>?): Boolean {
         return other is DepartureItem &&
                 departure == other.departure
+    }
+
+    companion object {
+        val dateFormatter = SimpleDateFormat("h:mm", Locale.US)
     }
 }
